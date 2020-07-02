a1d470f10a74bdbdc70fd1166d9f9f917d71a170
Joshua Lowe <joshualowe1002@gmail.com>
2020-07-02 22:31:34 +0100

    Wider start page

--- ui/src/views/ImageModal.tsx
+++ ui/src/views/ImageModal.tsx
@@ -40,7 +40,7 @@ export default class ImageModal<T extends ImageModalOption> extends Component<Im
       <div class='modal'>
       <input id='modal_1' type='checkbox' disabled={true} checked={this.props.visible} />
       <label for='modal_1' class='overlay'></label>
-      <div id="wrapper" style="width: 49% !important">
+      <div id="wrapper" style="width: 54% !important">
         <div id="left">
           <div id="inner-div-image">
             <img id="mode-logo" src="images/weblogo.png"></img>
