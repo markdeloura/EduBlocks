54245fdfc73b27448c719f7e2869b28a26eaa826
Mark DeLoura <mdeloura@satori.org>
2021-06-30 11:50:30 -0700

    Removed platforms not CircuitPython from selector

--- ui/src/platforms/index.ts
+++ ui/src/platforms/index.ts
@@ -1,17 +1,17 @@
 import { Platform, PlatformInterface, PlatformSelection } from '../types';
-import { newCalliopePlatform } from './calliope';
+// import { newCalliopePlatform } from './calliope';
 import { newCircuitPythonPlatform } from './circuitpython';
-import { newMicrobitPlatform } from './microbit';
-import { newRaspberryPiPlatform } from './raspberrypi';
-import { newWebPlatform } from './python';
+// import { newMicrobitPlatform } from './microbit';
+// import { newRaspberryPiPlatform } from './raspberrypi';
+// import { newWebPlatform } from './python';
 
 
 export function getPlatformList(): PlatformSelection[] {
   return [
-    { platform: 'Python', title: 'Python 3', image: '/images/webpy.png', help: 'https://edublocks.org/python.html' },
+    // { platform: 'Python', title: 'Python 3', image: '/images/webpy.png', help: 'https://edublocks.org/python.html' },
     // { title: 'Advanced Python', image: '/images/advpy.png', help: 'https://edublocks.org' },
-    { platform: 'RaspberryPi', title: 'Raspberry Pi', image: '/images/pi.png', help: 'https://edublocks.org/pi.html' },
-    { platform: 'MicroBit', title: 'micro:bit', image: '/images/microbit.png', help: 'https://edublocks.org/microbit.html' },
+    // { platform: 'RaspberryPi', title: 'Raspberry Pi', image: '/images/pi.png', help: 'https://edublocks.org/pi.html' },
+    // { platform: 'MicroBit', title: 'micro:bit', image: '/images/microbit.png', help: 'https://edublocks.org/microbit.html' },
     { platform: 'CircuitPython', title: 'CircuitPython', image: '/images/circuitplayground.png', help: 'https://edublocks.org/circuitpy.html' },
     // { platform: 'Calliope', title: 'Calliope Mini', image: '/images/calliope.png', help: 'https://docs.edublocks.org/mode-guides/calliope' },
   ];
@@ -19,16 +19,19 @@ export function getPlatformList(): PlatformSelection[] {
 
 export async function getPlatform(platform: Platform): Promise<PlatformInterface> {
   switch (platform) {
-    case 'Python':
+/*    case 'Python':
       return newWebPlatform();
     case 'MicroBit':
       return newMicrobitPlatform(); 
     case 'RaspberryPi':
       return newRaspberryPiPlatform();
+*/
     case 'CircuitPython': 
       return newCircuitPythonPlatform();
+/*
     case 'Calliope':
       return newCalliopePlatform();
+*/
     default:
       throw new Error('Invalid platform: ' + platform);
       
