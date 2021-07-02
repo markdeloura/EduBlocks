30c6d616a8de0dd600f42714da082313bc422444
Mark DeLoura <mdeloura@satori.org>
2021-07-01 17:46:59 -0700

    Added FESimData init and fixed import

--- ui/src/blocks/circuitpython/fesim/definitions.ts
+++ ui/src/blocks/circuitpython/fesim/definitions.ts
@@ -1,16 +1,86 @@
 export default function define(Blocks: Blockly.BlockDefinitions) {
 
-    var maincolour = "#ff0066";
+    var fesimcolour = "#ff6666";
     
-    Blocks['JUNK_JUNK'] = {
-      init: function () {
-        this.appendDummyInput()
-          .appendField('import board');
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
-        this.setColour(maincolour);
-        this.setTooltip('Imports the board library.');
-        this.setHelpUrl('http://www.example.com/');
-      },
+    Blocks['fesim_import'] = {
+        init: function () {
+            this.appendDummyInput().appendField('import FE_sim_data');
+            this.setPreviousStatement(true, null);
+            this.setNextStatement(true, null);
+            this.setColour(fesimcolour);
+            this.setTooltip('Imports the FE Sim Data library.');
+            this.setHelpUrl('http://www.futureengineers.org/');
+        },
+    };
+
+    Blocks['fesim_init_provider'] = {
+        init: function () {
+            this.appendDummyInput()
+                .appendField(new Blockly.FieldVariable("sim_data"), "var")
+                .appendField(" = FE_sim_data.FESimData(provider='")
+                .appendField(new Blockly.FieldDropdown([['raven_aerostar', 'raven_aerostar'], ['up_aerospace', 'up_aerospace'], ["blue_origin", "blue_origin"]]), "provider")
+            this.appendDummyInput()
+                .appendField("')");            
+            this.setPreviousStatement(true, null);
+            this.setNextStatement(true, null);
+            this.setColour(fesimcolour);
+            this.setTooltip('Initializes module with the flight provider.');
+            this.setHelpUrl('http://www.futureengineers.org/');
+        },
+    };
+
+    Blocks['fesim_update'] = {
+        init: function () {
+            this.appendDummyInput().appendField('import board');
+            this.setPreviousStatement(true, null);
+            this.setNextStatement(true, null);
+            this.setColour(fesimcolour);
+            this.setTooltip('Imports the board library.');
+            this.setHelpUrl('http://www.futureengineers.org/');
+        },
+    };
+
+    Blocks['fesim_streaming'] = {
+        init: function () {
+            this.appendDummyInput().appendField('import board');
+            this.setPreviousStatement(true, null);
+            this.setNextStatement(true, null);
+            this.setColour(fesimcolour);
+            this.setTooltip('Imports the board library.');
+            this.setHelpUrl('http://www.futureengineers.org/');
+        },
+    };
+        
+    Blocks['fesim_new_data'] = {
+        init: function () {
+            this.appendDummyInput().appendField('import board');
+            this.setPreviousStatement(true, null);
+            this.setNextStatement(true, null);
+            this.setColour(fesimcolour);
+            this.setTooltip('Imports the board library.');
+            this.setHelpUrl('http://www.futureengineers.org/');
+        },
+    };
+
+    Blocks['fesim_data'] = {
+        init: function () {
+            this.appendDummyInput().appendField('import board');
+            this.setPreviousStatement(true, null);
+            this.setNextStatement(true, null);
+            this.setColour(fesimcolour);
+            this.setTooltip('Imports the board library.');
+            this.setHelpUrl('http://www.futureengineers.org/');
+        },
+    };
+
+    Blocks['fesim_event'] = {
+        init: function () {
+            this.appendDummyInput().appendField('import board');
+            this.setPreviousStatement(true, null);
+            this.setNextStatement(true, null);
+            this.setColour(fesimcolour);
+            this.setTooltip('Imports the board library.');
+            this.setHelpUrl('http://www.futureengineers.org/');
+        },
     };
 }
--- ui/src/blocks/circuitpython/fesim/generators.ts
+++ ui/src/blocks/circuitpython/fesim/generators.ts
@@ -1,6 +1,42 @@
 export default function define(Python: Blockly.BlockGenerators) {
-    Python['JUNK_JUNK'] = function (block) {
-      const code = 'import board\n';
-      return code;
+    Python['fesim_import'] = function (block) {
+        const code = 'import FE_sim_data\n';
+        return code;
     };
+
+    Python['fesim_init_provider'] = function (block) {
+        // Get parameters
+        let variable_name = Blockly.Python.variableDB_.getName(block.getFieldValue('var'), Blockly.Variables.NAME_TYPE);
+        let provider_name = block.getFieldValue('provider');
+        
+        // Assemble Python into code variable.
+        let code = variable_name + " = FE_sim_data.FESimData(provider='" + provider_name + "')\n";
+        return code;
+    };
+
+    Python['fesim_update'] = function (block) {
+        const code = 'import board\n';
+        return code;
+    };
+
+    Python['fesim_streaming'] = function (block) {
+        const code = 'import board\n';
+        return code;
+    };
+
+    Python['fesim_new_data'] = function (block) {
+        const code = 'import board\n';
+        return code;
+    };
+
+    Python['fesim_data'] = function (block) {
+        const code = 'import board\n';
+        return code;
+    };
+
+    Python['fesim_event'] = function (block) {
+        const code = 'import board\n';
+        return code;
+    };            
 }
+
